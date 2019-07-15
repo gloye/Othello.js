d43b63a31b5b60e134a884ad2fbb70d6ac730ef9
sunsx <sunsx@3721zh.com>
2019-07-15 17:16:28 +0800

    no message

--- othello2.js
+++ othello2.js
@@ -1,4 +1,4 @@
-const model = new Array(64).fill(0)
+const model = new Array(64).fill(0);
 
 // 初始化
 const initDiscs = [
@@ -22,132 +22,143 @@ const initDiscs = [
     y: 4,
     status: 1
   }
-]
+];
 
 initDiscs.forEach(({ x, y, status }) => {
-  const index = y * 8 + x
-  model[index] = status
-})
+  const index = y * 8 + x;
+  model[index] = status;
+});
 
 // 下一个
-const nextStatus = 1
+const nextStatus = 1;
 
 // 分棋子
-const oppositeDiscs = []
+const oppositeDiscs = [];
 
 model.forEach((status, index) => {
-  if (status === 3 - nextStatus) oppositeDiscs.push(index)
-})
+  if (status === 3 - nextStatus) oppositeDiscs.push(index);
+});
 
 // helper 辅助函数
-const getModelIndex = ([x, y]) => x + y * 8
+const coordinateToIndex = (x, y) => x + y * 8;
+const indexToCoordinate = i => [i % 8, (i - (i % 8)) / 8];
 
-const check = ([x, y]) => {
-  const index = getModelIndex([x, y])
-  if (model[index] === 0) {
-    return false
-  }
-  if (model[index] === 1) {
-    discsToChange.push([x, y])
-  }
-  if (model[index] === 2) {
-    canMove = true
-    return false
-  }
-}
-const checkActions = ([x, y]) => {
-  const actions = new Map(
+const actionMap = (position = [0, 0]) => {
+  const [x, y] = position;
+  return new Map([
     [
       [x - 1, y + 1],
-      function leftTop() {
-        while ((x < 8, y > 0)) {
-          if (check([x++, y--])) break
+      function leftTop(fn) {
+        let [m, n] = [x, y];
+        while (m < 8 && n > 0) {
+          if (fn(m++, n--)) break;
         }
       }
     ],
     [
       [x - 1, y],
-      function left() {
-        while (x < 8) {
-          if (check([x++, y])) break
+      function left(fn) {
+        let [m, n] = [x, y];
+        while (m < 8) {
+          if (fn(m++, n)) break;
         }
       }
     ],
     [
       [x - 1, y - 1],
-      function leftBottom() {
-        while ((x <= 8, y <= 8)) {
-          if (check([x++, y++])) break
+      function leftBottom(fn) {
+        let [m, n] = [x, y];
+        while (m < 8 && n < 8) {
+          if (!fn(++m, ++n)) break;
         }
       }
     ],
     [
       [x, y + 1],
-      function top() {
-        while (y >= 0) {
-          if (check([x, y--])) break
+      function top(fn) {
+        let [m, n] = [x, y];
+        while (n >= 0) {
+          if (fn(m, n--)) break;
         }
       }
     ],
     [
       [x, y - 1],
-      function bottom() {
-        while (y < 8) {
-          if (check([x, y++])) break
+      function bottom(fn) {
+        let [m, n] = [x, y];
+        while (n < 8) {
+          if (fn(m, n++)) break;
         }
       }
     ],
     [
       [x + 1, y + 1],
-      function rightTop() {
-        while ((x >= 0, y >= 0)) {
-          if (check(x--, y--)) break
+      function rightTop(fn) {
+        let [m, n] = [x, y];
+        while (m >= 0 && n >= 0) {
+          if (fn(x--, y--)) break;
         }
       }
     ],
     [
       [x + 1, y],
       function right() {
-        while (x >= 0) {
-          if (check(x--, y)) break
+        let [m, n] = [x, y];
+        while (m >= 0) {
+          if (fn(m--, n)) break;
         }
       }
     ],
     [
       [x + 1, y - 1],
       function rightBottom() {
-        while ((x >= 0, y < 8)) {
-          if (check(x--, y++)) break
+        let [m, n] = [x, y];
+        while (m >= 0 && n < 8) {
+          if (fn(m--, n++)) break;
         }
       }
     ]
-  )
+  ]);
+};
+
+function checkPosition([x, y]) {
+  if (x < 0 && x >= 8 && y < 0 && y >= 8) return false; // 边界处理
+  if (model[coordinateToIndex(x, y)] !== 0) return false; // 非空处理
+  return true;
 }
 
 // 找关联 定边界
-const findDiscsCanMove = function(i) {
-  let x = i % 8
-  let y = (i - (i % 8)) / 8
-  const aroundDiscs = [
-    [x - 1, y + 1],
-    [x - 1, y],
-    [x - 1, y - 1],
-    [x, y + 1],
-    [x, y - 1],
-    [x + 1, y + 1],
-    [x + 1, y],
-    [x + 1, y - 1]
-  ]
-  const relDiscs = aroundDiscs
-    .filter(disc => disc[0] >= 0 && disc[1] >= 0 && disc[0] < 8 && disc[1] < 8) // 边界处理
-    .filter(disc => model[getIndex(disc)] === 0) // 空值
-    .filter(disc => checkDisc(disc)) // 可以落子
-  return relDiscs
-}
+const findDiscsActive = function(i) {
+  const [x, y] = indexToCoordinate(i);
+  let discs = [i];
+  let resolve = false;
+  function checkStatus(x, y) {
+    const index = coordinateToIndex(x, y);
+    if (model[index] === 1) {
+      discs.push(index);
+      return true;
+    }
+    if (model[index] === 2) {
+      resolve = true;
+    }
+    return false;
+  }
+  const newMap = actionMap([x, y]);
+  newMap.forEach((handle, key) => {
+    if (checkPosition(key)) {
+      handle && handle(checkStatus);
+    }
+  });
+  if(resolve){
+    return discs
+  }
+  return []
+};
 
-//
-const discs = []
+// findDiscsActive(oppositeDiscs[0]);
+
+/* const discs = [];
 
 oppositeDiscs.forEach(
   disc => (discs = [...new Set([...discs, ...findDiscsCanMove(disc)])])
-)
+); */
